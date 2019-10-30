1876842bea99c558135e6eb71bc61b26a7192514
sbx_user1051 <sbx_user1051@169.254.103.189>
2019-10-30 21:42:41 +0000

    Wed Oct 30 21:42:41 UTC 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -2,8 +2,9 @@ package befaster.solutions.CHL;
 
 public class CheckliteSolution {
     public Integer checklite(String skus) {
-        System.out.println(skus);
+        System.out.println(skus); // need to see the input format for this function
         return -1;
     }
 }
+
 
